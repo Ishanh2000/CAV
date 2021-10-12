4d6e826294dfe560643cbb03d4017d965b23adaf
Ishanh2000 <misraishanh200@gmail.com>
2021-10-13 02:05:12 +0530

    Future Path Complete. Granularity parametr = b

--- dijkstra.py
+++ dijkstra.py
@@ -15,21 +15,21 @@ def Dijkstra(graph, src, dst):
   l_WPs = len(graph["waypoints"])
   NBs = graph["neighbours"]
 
-  d = [float('inf') for i in range(l_WPs)] # distances
+  cost = [float('inf') for i in range(l_WPs)] # costs are in time (milliseconds)
   parent = [None for i in range(l_WPs)] # parents
-  d[src] = 0 # distance from source = 0
+  cost[src] = 0 # cost from source = 0
   Q = [i for i in range(l_WPs)] # all vertices in Min-Priority Queue
 
   while len(Q) > 0:
-    min_q_idx = np.array([d[u] for u in Q]).argmin() # get vertex with smallest distance
+    min_q_idx = np.array([cost[u] for u in Q]).argmin() # get vertex with smallest distance
     u = Q[min_q_idx]
     del Q[min_q_idx]
     for nb in NBs[u]:
       v, wt = nb["idx"], nb["wt"]
       if v in Q:
-        alt = d[u] + wt
-        if alt < d[v]:
-          d[v] = alt
+        alt = cost[u] + wt
+        if alt < cost[v]:
+          cost[v] = alt
           parent[v] = u
   
   sp = [dst] # list of vertices
@@ -38,6 +38,7 @@ def Dijkstra(graph, src, dst):
   sp.reverse()
   return sp
 
+b = 0.5 # granularity in metres
 
 def compute_future_path(graph, car, d_max):
   """ Computing Future Path of a Car """
@@ -45,21 +46,31 @@ def compute_future_path(graph, car, d_max):
 
   WPs = graph["waypoints"]
   sp = Dijkstra(graph = graph, src = car["closest_wp"], dst = car["dest"])
+  l_sp = len(sp)
 
-  # fp, tot_dist = [sp[0]], 0
+  fp, total_dist = [], 0.0 # future points, total distance (cm) covered by these future points
+  for i in range(1 , l_sp):
+    rem = (100.0 * d_max) - total_dist # remaining distance (cm)
+    e_start, e_end = WPs[sp[i-1]], WPs[sp[i]] # points of edge
+    d = dist(e_start, e_end) # edge length (cm)
+    num_fp = np.ceil(np.min([rem, d]) / (b * 100))
+    print(num_fp)
+    for j in range(int(num_fp)):
+      fp.append({
+        "x" : ( (d-100*j*b)*e_start["x"] + 100*j*b*e_end["x"] ) / d,
+        "y" : ( (d-100*j*b)*e_start["y"] + 100*j*b*e_end["y"] ) / d,
+      })
+    if (rem <= d):
+      break
+    if i == (l_sp-1):
+      fp.append(e_end)
+      break
+    total_dist += d
 
-  # for u in sp[1:]:
-  #   edge_len = dist(WPs[fp[-1]], WPs[u])
-  #   if (tot_dist + edge_len) >= (100 * d_max): # LHS is in cm
-  #     break
-  #   fp.append(u)
-  #   tot_dist += edge_len
-
-  return sp
+  return fp, sp
 
 
-
-graph_json_file = "./samples/eye.json"
+graph_json_file = "./samples/hex.json"
 
 if __name__ == "__main__":
   G = []
@@ -68,10 +79,10 @@ if __name__ == "__main__":
 
   cars = G["cars"]; l_cars = len(cars)
 
-  for ci in range(l_cars):
-    cars[ci]["sp"] = Dijkstra(G, cars[ci]["closest_wp"], cars[ci]["dest"])
-    # cars[ci]["fp"] = compute_future_path(graph = G, car = cars[ci], d_max = 10)
-    print(f"ID = {cars[ci]['id']}, FP = {cars[ci]['sp']}")
-  
+  for ci in range(l_cars-1):
+    cars[ci]["fp"], cars[ci]["sp"] = compute_future_path(graph = G, car = cars[ci], d_max = 10)
+    print(f"ID = {cars[ci]['id']}, SP = {cars[ci]['sp']}")
+    print(f"FP = {cars[ci]['fp']}")
+    print()
 
 
